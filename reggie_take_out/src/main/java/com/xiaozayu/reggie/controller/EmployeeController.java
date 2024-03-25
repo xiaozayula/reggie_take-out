7dc823014ed92af3bce9e8edf70f2034926f1dcd
xiaozayula <981446636@qq.com>
2024-03-25 15:10:12 +0800

    根据id查询员工信息

--- reggie_take_out/src/main/java/com/xiaozayu/reggie/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/xiaozayu/reggie/controller/EmployeeController.java
@@ -112,4 +112,13 @@ public class EmployeeController {
         employeeService.updateById(employee);
         return  R.success("员工信息修改成功");
     }
+    @GetMapping("/{id}")
+    public  R<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息");
+        Employee employee=employeeService.getById(id);
+        if(employee!=null){
+            return  R.success(employee);
+        }
+        return R.error("没有查询到员工");
+    }
 }
