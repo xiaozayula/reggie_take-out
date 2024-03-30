38c22eaca55fd432cbbf67ce417f6d79f7f4e9ae
xiaozayula <981446636@qq.com>
2024-03-30 18:44:41 +0800

    文件下载

--- reggie_take_out/src/main/java/com/xiaozayu/reggie/controller/CommonController.java
+++ reggie_take_out/src/main/java/com/xiaozayu/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.xiaozayu.reggie.controller;
 import com.xiaozayu.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -53,4 +58,30 @@ public class CommonController {
         }
         return R.success(fileName);
     }
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        try {
+            //通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath+name));
+            //通过输出流再把文件写回给浏览器
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len=0;
+            byte[] bytes = new byte[1024];
+
+            while ((len=fileInputStream.read(bytes))!=-1){//fileInputStream.read(bytes)从文件中读取字节并存储在指定的数组中
+                outputStream.write(bytes,0,len);
+                outputStream.flush();//刷新
+            }
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- reggie_take_out/src/main/java/com/xiaozayu/reggie/mapper/CategoryMapper.java
+++ reggie_take_out/src/main/java/com/xiaozayu/reggie/mapper/CategoryMapper.java
@@ -2,6 +2,7 @@ package com.xiaozayu.reggie.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xiaozayu.reggie.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,5 +11,6 @@ import com.xiaozayu.reggie.entity.Category;
  * @Date: 2024/03/26/15:25
  * @Description:
  */
+@Mapper
 public interface CategoryMapper extends BaseMapper<Category> {
 }
